a94e165b94f4b75050bc0822ab07a20c66dcdc87
LukaKemenovic <kemenovic.luka@gmail.com>
2023-01-11 23:03:04 +0100

    Markers update -added a  new layout for marker-icon.png - developed two prototype location markers

--- Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
@@ -1,25 +1,64 @@
 package hr.foi.air.car2car
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.os.Bundle
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import android.view.LayoutInflater
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     //creating variable for mapFragment
-    lateinit var mapFragment : SupportMapFragment
-    lateinit var googleMap: GoogleMap
+    private lateinit var mapFragment : SupportMapFragment
+    private lateinit var googleMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
-        mapFragment.getMapAsync(OnMapReadyCallback {
+        setupMap()
+    }
 
-        })
+    private fun setupMap(){
+        mapFragment = supportFragmentManager.findFragmentById(R.id.mainMap) as SupportMapFragment
+        mapFragment !!.getMapAsync(this)
+    }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        val latLongCar1 = LatLng(46.308007, 16.358387)
+        val latLongCar2 = LatLng(46.307881, 16.358226)
+
+        val markerView = (getSystemService(LAYOUT_INFLATER_SERVICE) as LayoutInflater).inflate(R.layout.marker_layout, null)
+        val cardView = markerView.findViewById<CardView>(R.id.markerCardView)
+
+        val bitmapCar1 = Bitmap.createScaledBitmap(viewToBitmap(cardView)!!, cardView.width, cardView.height, false)
+        val markerIconCar1 = BitmapDescriptorFactory.fromBitmap(bitmapCar1)
+        googleMap.addMarker(MarkerOptions().position(latLongCar1).icon(markerIconCar1))
+
+        val bitmapCar2 = Bitmap.createScaledBitmap(viewToBitmap(cardView)!!, cardView.width, cardView.height, false)
+        val markerIconCar2 = BitmapDescriptorFactory.fromBitmap(bitmapCar2)
+        googleMap.addMarker(MarkerOptions().position(latLongCar2).icon(markerIconCar2))
+
+        val cameraPosition = CameraPosition.Builder().target(latLongCar1).zoom(18.0f).build()
+        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        //googleMap.moveCamera(cameraUpdate)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLongCar1, 19f))
+    }
+
+    private fun viewToBitmap(view : View): Bitmap {
+        view.measure(View.MeasureSpec.UNSPECIFIED, View.MeasureSpec.UNSPECIFIED)
+        val bitmap = Bitmap.createBitmap(view.measuredWidth, view.measuredHeight, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        view.layout(0,0,view.measuredWidth, view.measuredHeight)
+        view.draw(canvas)
+        return bitmap
     }
 }
