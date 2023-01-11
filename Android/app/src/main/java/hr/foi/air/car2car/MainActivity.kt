85e15985bed1417392dc7513294289aaa6967184
LukaKemenovic <kemenovic.luka@gmail.com>
2023-01-11 19:17:50 +0100

    Map Update -added dependecies - entered google api - fixed al gradle settings - MainActivity = MapView - added "fragment" to activity_main.xml

--- Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
@@ -2,10 +2,24 @@ package hr.foi.air.car2car
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
 class MainActivity : AppCompatActivity() {
+
+    //creating variable for mapFragment
+    lateinit var mapFragment : SupportMapFragment
+    lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map_fragment) as SupportMapFragment
+        mapFragment.getMapAsync(OnMapReadyCallback {
+
+        })
+
     }
 }
