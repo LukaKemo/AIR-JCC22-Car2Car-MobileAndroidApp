9adf86d842fcb37b5c5607a0f0d2dcff35815746
LukaKemenovic <kemenovic.luka@gmail.com>
2023-01-12 20:03:18 +0100

    Connection to MQTT - added dependencies for MQTT - fixed settings for MQTT on manifest - added activity_main.xml code - strings + -inserted main code for connection to MQTT in MainActivity.kt

--- Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
+++ Android/app/src/main/java/hr/foi/air/car2car/MainActivity.kt
@@ -9,22 +9,27 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.google.android.gms.maps.*
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.hivemq.client.mqtt.MqttClient
+import com.hivemq.client.mqtt.MqttGlobalPublishFilter
+import java.nio.charset.StandardCharsets.UTF_8
 
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     //creating variable for mapFragment
     private lateinit var mapFragment : SupportMapFragment
-    private lateinit var googleMap: GoogleMap
+    //private lateinit var googleMap : GoogleMap
+    //private lateinit var client : MqttAndroidClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupMap()
+
+        initializationMQTT()
     }
 
     private fun setupMap(){
@@ -47,8 +52,8 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         val markerIconCar2 = BitmapDescriptorFactory.fromBitmap(bitmapCar2)
         googleMap.addMarker(MarkerOptions().position(latLongCar2).icon(markerIconCar2))
 
-        val cameraPosition = CameraPosition.Builder().target(latLongCar1).zoom(18.0f).build()
-        val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
+        //val cameraPosition = CameraPosition.Builder().target(latLongCar1).zoom(18.0f).build()
+        //val cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition)
         //googleMap.moveCamera(cameraUpdate)
         googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLongCar1, 19f))
     }
@@ -60,5 +65,51 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         view.layout(0,0,view.measuredWidth, view.measuredHeight)
         view.draw(canvas)
         return bitmap
+    }
+
+    private fun initializationMQTT() {
+    // create an MQTT client
+        val host = "e5c6690c23234ff8b2e11e59d3fb82be.s2.eu.hivemq.cloud"
+        val username = "JCC_RC_CommunicationFramework"
+        val password = "@B1EfbPKD%Pp%kPG"
+
+        // create an MQTT client
+        val client = MqttClient.builder()
+            .useMqttVersion5()
+            .serverHost(host)
+            .serverPort(8883)
+            .sslWithDefaultConfig()
+            .buildBlocking()
+
+        // connect to HiveMQ Cloud with TLS and username/pw
+        client.connectWith()
+            .simpleAuth()
+            .username(username)
+            .password(UTF_8.encode(password))
+            .applySimpleAuth()
+            .send()
+        println("Connected successfully")
+
+        // subscribe to the topic "my/test/topic"
+        client.subscribeWith()
+            .topicFilter("LOCATION")
+            .send()
+
+        // subscribe to the topic "my/test/topic"
+        client.subscribeWith()
+            .topicFilter("LOCATION")
+            .send()
+        // set a callback that is called when a message is received (using the async API style)
+        client.toAsync().publishes(MqttGlobalPublishFilter.ALL) { publish ->
+            println("Received message: ${publish.topic} -> ${UTF_8.decode(publish.payload.get())}")
+
+            // disconnect the client after a message was received
+            client.disconnect()
+        }
+        // publish a message to the topic "my/test/topic"
+        client.publishWith()
+            .topic("LOCATION")
+            .payload(UTF_8.encode("Hello"))
+            .send()
     }
 }
