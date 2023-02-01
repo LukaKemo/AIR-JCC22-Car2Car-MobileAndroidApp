fccd56aba947c57bd0ca9154386f8c2be960fdea
LukaKemenovic <kemenovic.luka@gmail.com>
2023-02-01 23:29:35 +0100

    Finished final use cases - finished settings - added dark/light mode - fixed icons and text

--- Android/app/src/main/java/hr/foi/air/car2car/SettingsActivity.kt
+++ Android/app/src/main/java/hr/foi/air/car2car/SettingsActivity.kt
@@ -4,12 +4,25 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val btn = findViewById<Switch>(R.id.switch1)
+        btn.setOnCheckedChangeListener { _, isChecked ->
+            if (btn.isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                btn.text = "Disable dark mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                btn.text = "Enable dark mode"
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
